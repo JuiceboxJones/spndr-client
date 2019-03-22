df312be36faeba258ea59fb045c9dd952b17ad6e
Juicebox Jones <jderhalli@gmail.com>
2019-03-22 14:14:09 -0700

    endpoint adjustment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://damp-wildwood-10487.herokuapp.com',
+  API_ENDPOINT: 'https://postgresql-aerodynamic-44273.herokuapp.com',
   TOKEN_KEY: process.env.TOKEN_KEY,
 }
