83f89077ce8d4180a8f8d1cdb0f49f10a128c347
Juicebox Jones <jderhalli@gmail.com>
2019-03-22 16:09:55 -0700

    endpoint adjustment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://fierce-castle-94350.herokuapp.com/api',
+  API_ENDPOINT:'https://fierce-castle-94350.herokuapp.com',
   TOKEN_KEY: process.env.TOKEN_KEY,
 }
