e1c3d9adfad9f57025d9e971d70046a62ff14250
Juicebox Jones <jderhalli@gmail.com>
2019-03-25 16:47:28 -0700

    endpoint corrected

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  // API_ENDPOINT:'https://immense-scrubland-59048.herokuapp.com',
-  API_ENDPOINT:'http://localhost:8000',
+  API_ENDPOINT:'https://immense-scrubland-59048.herokuapp.com',
   TOKEN_KEY: process.env.TOKEN_KEY,
   
 }
