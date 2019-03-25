cf731a8bcfba50903af288ff1d8846700b092123
Juicebox Jones <jderhalli@gmail.com>
2019-03-25 16:24:52 -0700

    redeploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT:'https://pure-cliffs-79091.herokuapp.com',
+  API_ENDPOINT:'https://immense-scrubland-59048.herokuapp.com/',
   TOKEN_KEY: process.env.TOKEN_KEY,
   
 }
